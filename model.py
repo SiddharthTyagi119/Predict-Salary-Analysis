0b8c0fc2b5dcd75c82e988944ca5dc0f51227249
Siddharth Tyagi <52122171+SiddharthTyagi119@users.noreply.github.com>
2020-10-07 01:31:16 +0530

    Update model.py

--- model.py
+++ model.py
@@ -32,8 +32,8 @@ regressor = LinearRegression()
 regressor.fit(X, y)
 
 # Saving model to disk
-pickle.dump(regressor, open('model1.pkl','wb'))
+pickle.dump(regressor, open('model.pkl','wb'))
 
 # Loading model to compare the results
-model = pickle.load(open('model1.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 print(model.predict([[2, 9, 6]]))
